d2ba9f1df70a8880cfd3a2046716784388f158d5
UsNNam <97780933+UsNNam@users.noreply.github.com>
2024-04-03 17:22:05 +0700

    Init project

---
+++ Simple_Paint/MainWindow.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Simple_Paint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ShapeToDraw curShape;
+        bool isDraw = false;
+        public static SolidColorBrush fillColorMain { get; set; }
+
+        public static SolidColorBrush borderColorMain { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShapeToDraw.canvas = canvas;
+            curShape = new LineShape(new Point(0, 0), new Point(0, 0));
+        }
+
+        private void LineButton_Click(object sender, RoutedEventArgs e)
+        {
+            curShape = new LineShape(new Point(0, 0), new Point(0, 0));
+            if(fillColorMain == null)
+            {
+                ShapeToDraw.fillColor = Brushes.Black;
+            }
+            else {                 
+                ShapeToDraw.fillColor = fillColorMain;
+                       
+            }
+           
+            if(borderColorMain == null)
+            {
+                ShapeToDraw.borderColor = Brushes.Black;
+            }
+            else
+            {
+                ShapeToDraw.borderColor = borderColorMain;
+            }
+        
+        }
+        private void EllipseButton_Click(object sender, RoutedEventArgs e)
+        {
+            curShape = new EllipseShape(new Point(0, 0), new Point(0, 0));
+            if (fillColorMain == null)
+            {
+                ShapeToDraw.fillColor = Brushes.Black;
+            }
+            else
+            {
+                ShapeToDraw.fillColor = fillColorMain;
+
+            }
+
+            if(borderColorMain == null)
+            {
+                ShapeToDraw.borderColor = Brushes.Black;
+            }
+            else
+            {
+                ShapeToDraw.borderColor = borderColorMain;
+            }
+        }
+        private void RectangleButton_Click(object sender, RoutedEventArgs e)
+        {
+            curShape = new RectangleShape(new Point(0, 0), new Point(0, 0));
+            if (fillColorMain == null)
+            {
+                ShapeToDraw.fillColor = Brushes.Black;
+            }
+            else
+            {
+                ShapeToDraw.fillColor = fillColorMain;
+
+            }
+
+            if(borderColorMain == null)
+            {
+                ShapeToDraw.borderColor = Brushes.Black;
+            }
+            else
+            {
+                ShapeToDraw.borderColor = borderColorMain;
+            }
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed && isDraw == false)
+            {
+                
+                Point point = e.GetPosition(canvas);
+                if(point != null) {
+                    curShape.StartPoint = point;
+                    curShape.EndPoint = point;
+                    curShape.Draw();
+                    isDraw = true;
+                }
+                
+            }
+            
+
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Released && isDraw)
+            {
+                curShape.EndPoint = e.GetPosition(canvas);
+                isDraw = false;
+            }
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraw)
+            {
+                if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                {
+                    if (curShape is EllipseShape || curShape is RectangleShape)
+                    {
+                        // I want the endpoint make the shape into circle
+
+                        Point curPoint = e.GetPosition(canvas);
+                        // Tính toán độ dài cạnh của hình vuông
+                        double sideLength = Math.Min(Math.Abs(curPoint.X - curShape.StartPoint.X), Math.Abs(curPoint.Y - curShape.StartPoint.Y));
+
+                        // Tính toán tọa độ của endPoint
+                        double endX = curShape.StartPoint.X + (curPoint.X > curShape.StartPoint.X ? sideLength : -sideLength);
+                        double endY = curShape.StartPoint.Y + (curPoint.Y > curShape.StartPoint.Y ? sideLength : -sideLength);
+
+                        // Tạo ra endPoint từ tọa độ tính toán được
+                        Point endPoint = new Point(endX, endY);
+
+                        curShape.EndPoint = endPoint;
+                        curShape.UpdateEndPoint();
+
+
+                    }
+
+                }
+                else
+                {
+                    curShape.EndPoint = e.GetPosition(canvas);
+                    curShape.UpdateEndPoint();
+                }
+            }
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FillColor_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            if (radioButton.IsChecked == true)
+            {
+                ShapeToDraw.fillColor = (SolidColorBrush)radioButton.Tag;
+                fillColorMain = (SolidColorBrush)radioButton.Tag;
+            }
+            if (curShape != null)
+            {
+                
+            }
+        }
+
+        private void BorderColor_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            if (radioButton.IsChecked == true)
+            {
+                ShapeToDraw.borderColor = (SolidColorBrush)radioButton.Tag;
+                borderColorMain = (SolidColorBrush)radioButton.Tag;
+            }
+            if (curShape != null)
+            {
+                
+            }
+        }
+    }
+
+
+}
---
+++ Simple_Paint/ShapeToDraw.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Simple_Paint
+{
+    class ShapeToDraw
+    {
+        public static Canvas canvas;
+
+
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+
+        public static SolidColorBrush fillColor { get; set;}
+
+        public static SolidColorBrush borderColor { get; set; }
+
+        public ShapeToDraw(Point startPoint, Point endPoint) 
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+        }
+
+       
+        virtual public void Draw()
+        {
+            
+        }
+        virtual public void UpdateEndPoint()
+        {
+
+        }
+
+    }
+
+    class LineShape : ShapeToDraw
+    {
+        Line line;
+       public LineShape(Point startPoint, Point endPoint) : base(startPoint, endPoint)
+       {
+
+       }
+       public override void Draw()
+       {
+            // Draw the line in canvas with start point and end point
+            line = new Line();
+            if(fillColor == null)
+            {
+                fillColor = Brushes.Black;
+            }
+            line.Stroke = fillColor;
+            line.X1 = StartPoint.X;
+            line.Y1 = StartPoint.Y;
+            line.X2 = EndPoint.X;
+            line.Y2 = EndPoint.Y;
+            canvas.Children.Add(line);
+
+
+       }
+        public override void UpdateEndPoint()
+        {
+            line.X1 = EndPoint.X;
+            line.Y1 = EndPoint.Y;
+        }
+
+    }
+
+    class EllipseShape : ShapeToDraw
+    {
+        Ellipse ellipse;
+        public EllipseShape(Point startPoint, Point endPoint) : base(startPoint, endPoint)
+        {
+
+        }
+        public override void Draw()
+        {
+            
+            ellipse = new Ellipse();
+            ellipse.Stroke = borderColor;
+            ellipse.Fill = fillColor;
+            ellipse.StrokeThickness = 2;
+            ellipse.Width = Math.Abs(EndPoint.X - StartPoint.X);
+            ellipse.Height = Math.Abs(EndPoint.Y - StartPoint.Y);
+            Canvas.SetLeft(ellipse, StartPoint.X);
+            Canvas.SetTop(ellipse, StartPoint.Y);
+            canvas.Children.Add(ellipse);
+
+        }
+
+        public override void UpdateEndPoint()
+        {
+            ellipse.Width = Math.Abs(EndPoint.X - StartPoint.X);
+            ellipse.Height = Math.Abs(EndPoint.Y - StartPoint.Y);
+
+            if(EndPoint.X < StartPoint.X)
+            {
+                Canvas.SetLeft(ellipse, EndPoint.X);
+            }
+            if(EndPoint.Y < StartPoint.Y)
+            {
+                Canvas.SetTop(ellipse, EndPoint.Y);
+            }
+        }
+    }
+
+    class RectangleShape : ShapeToDraw
+    {
+        Rectangle rectangle;
+        public RectangleShape(Point startPoint, Point endPoint) : base(startPoint, endPoint)
+        {
+
+        }
+
+        public override void Draw()
+        {
+            rectangle = new System.Windows.Shapes.Rectangle();
+            rectangle.Stroke = borderColor;
+            rectangle.Fill = fillColor;
+            rectangle.StrokeThickness = 2;
+            rectangle.Width = Math.Abs(EndPoint.X - StartPoint.X);
+            rectangle.Height = Math.Abs(EndPoint.Y - StartPoint.Y);
+            Canvas.SetLeft(rectangle, StartPoint.X);
+            Canvas.SetTop(rectangle, StartPoint.Y);
+            canvas.Children.Add(rectangle);
+        }
+
+        public override void UpdateEndPoint()
+        {
+            rectangle.Width = Math.Abs(EndPoint.X - StartPoint.X);
+            rectangle.Height = Math.Abs(EndPoint.Y - StartPoint.Y);
+            if(EndPoint.X < StartPoint.X)
+            {
+                Canvas.SetLeft(rectangle, EndPoint.X);
+            }
+            if(EndPoint.Y < StartPoint.Y)
+            {
+                Canvas.SetTop(rectangle, EndPoint.Y);
+            }
+
+        }
+    }
+}

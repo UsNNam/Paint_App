2abca09b4ba95ca974333fe13ca780b1024f67fc
lenguyenthai123 <21127162@student.hcmus.edu.vn>
2024-04-16 15:22:37 +0700

    Merge

--- Simple_Paint/obj/Debug/net8.0-windows/Simple_Paint.AssemblyInfo.cs
+++ Simple_Paint/obj/Debug/net8.0-windows/Simple_Paint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simple_Paint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b5479a51a710519aa2c925a2414aba3919f90d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0656b33c80e39ae755f47ffcb91c251e70a5c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simple_Paint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simple_Paint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
